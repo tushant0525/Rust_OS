d656d535aefcab5318cc0039785ee36dc4f4b1b0
tushant0525 <tushant0525@gmail.com>
2024-03-11 02:02:45 +0530

    Implemented A  Simple Executor for async/await tasks

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,11 @@
 
 extern crate alloc;
 
-use alloc::{boxed::Box, vec, vec::Vec, rc::Rc};
 use bootloader::{entry_point, BootInfo};
 use core::panic::PanicInfo;
 use rust_os::memory::{self, BootInfoFrameAllocator};
+use rust_os::task::simple_executor::SimpleExecutor;
+use rust_os::task::Task;
 use rust_os::{allocator, println};
 use x86_64::VirtAddr;
 
@@ -25,23 +26,9 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
 
     allocator::init_heap(&mut mapper, &mut frame_allocator).expect("heap initialization failed");
 
-    // allocate a number on the heap
-    let heap_value = Box::new(41);
-    println!("heap_value at {:p}", heap_value);
-
-    //create a dynamically sized vector
-    let mut vec = Vec::new();
-    for i in 0..500 {
-        vec.push(i);
-    }
-    println!("vec at {:p}", vec.as_slice());
-
-    //create a reference counted vector -> will be freed when count reaches 0
-    let reference_counted= Rc::new(vec![1,2,3]);
-    let cloned_reference = reference_counted.clone();
-    println!("current reference count is {}", Rc::strong_count(&cloned_reference));
-    core::mem::drop(reference_counted);
-    println!("reference count is {} now", Rc::strong_count(&cloned_reference));
+    let mut executor = SimpleExecutor::new();
+    executor.spawn(Task::new(example_task()));
+    executor.run();
 
     #[cfg(test)]
     test_main();
@@ -62,4 +49,12 @@ fn panic(info: &PanicInfo) -> ! {
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     rust_os::test_panic_handler(info)
+}
+async fn async_number() -> u32 {
+    42
+}
+
+async fn example_task() {
+    let number = async_number().await;
+    println!("async number: {}", number);
 }
